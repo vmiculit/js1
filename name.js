f4afd6d9194b7cf8e1547c69d3ed48ea10146ba5
Valentin Miculit <valsbox@gmail.com>
2016-01-18 10:58:22 -0700

    done

--- name.js
+++ name.js
@@ -1,12 +1,2 @@
-
-window.onload = function() {
-  myNameFunction();
-}
-
-function myNameFunction () {
-	var name = prompt("Please enter your name", "Mr. Awesome!");
-
-	if (person != null) {
-		document.getElementById ("name") .innerHTML = "Hello" + person + "!";
-	}
-}
+var name = prompt("Please enter your name", "Mr. Awesome!");
+alert("Hello " + name + "!")
